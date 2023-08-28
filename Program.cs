b7d5778e5f920e04fe1d8827e7c2c46b75c485ef
MarcelRuiz20 <143234045+MarcelRuiz20@users.noreply.github.com>
2023-08-28 13:23:45 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-double mont_consumo,mont_descuento,precio_descontado,impuesto,total_a_pagar;
-Console.WriteLine("Ingrese el monto de su consumo ");
-mont_consumo = double.Parse(Console.ReadLine());
-if (mont_consumo <= 100)
+Console.WriteLine("Hello, World!");
+int num1, num2, num3;
+Console.WriteLine("Ingrese tres numeros enteros de distinto valor.");
+num1 = int.Parse(Console.ReadLine());
+num2 = int.Parse(Console.ReadLine());
+num3 = int.Parse(Console.ReadLine());
+if (num1 > num2 && num1 > num3)
 {
-    mont_descuento = mont_consumo * 0.1;
-    precio_descontado = mont_consumo - mont_descuento;
-    impuesto = precio_descontado * 0.18;
-    total_a_pagar= precio_descontado + impuesto;
-    Console.WriteLine("Factura:");
-    Console.WriteLine("********");
-    Console.WriteLine("El monto del descuento es: "+mont_descuento);
-    Console.WriteLine("El impuesto es: "+impuesto);
-    Console.WriteLine("El sub total es: "+precio_descontado);
-    Console.WriteLine("El importe a pagar es: "+total_a_pagar);
+    Console.WriteLine("El numero mayor es: " + num1);
 }
-if (mont_consumo > 100)
+else if (num2 > num1 && num2 > num3)
 {
-    mont_descuento = mont_consumo * 0.2;
-    precio_descontado = mont_consumo - mont_descuento;
-    impuesto = precio_descontado * 0.18;
-    total_a_pagar = precio_descontado + impuesto;
-    Console.WriteLine("Factura:");
-    Console.WriteLine("********");
-    Console.WriteLine("El monto del descuento es: " + mont_descuento);
-    Console.WriteLine("El impuesto es: " + impuesto);
-    Console.WriteLine("El sub total es: " + precio_descontado);
-    Console.WriteLine("El importe a pagar es: " + total_a_pagar);
+    Console.WriteLine("El numero mayor es: " + num2);
+}
+else if (num3 > num1 && num3 > num2)
+{
+    Console.WriteLine("El numero mayor es: " + num3);
 }
