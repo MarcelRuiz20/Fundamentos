77864eab05e5852c5c9fb0372104b62320f68cf6
MarcelRuiz20 <143234045+MarcelRuiz20@users.noreply.github.com>
2023-09-17 21:02:24 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int Base,exponente,resultado;
-Console.Write("Ingrese la base: ");
-Base = int.Parse(Console.ReadLine());
-Console.Write("Ingrese el exponente: ");
-exponente = int.Parse(Console.ReadLine());
-resultado = 1;
-for (int i = 0; i < exponente; i++)
+int num_notas;
+float promedio_final,nota, suma_notas;
+Console.Write("Ingrese el número de notas: ");
+num_notas = int.Parse(Console.ReadLine());
+suma_notas = 0;
+for (int i = 1; i <= num_notas; i++)
 {
-    resultado = resultado * Base;
+    Console.Write("Ingrese la nota " + i + ": ");
+    nota = int.Parse(Console.ReadLine());
+    suma_notas = suma_notas + nota;
 }
-Console.WriteLine("La potencia es: " + resultado);
+promedio_final = suma_notas / num_notas;
+Console.WriteLine("El promedio es " + promedio_final);
