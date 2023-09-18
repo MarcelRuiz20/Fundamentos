e99c76b5edf7ce4451aca6e6ec9e2d79b9879140
MarcelRuiz20 <143234045+MarcelRuiz20@users.noreply.github.com>
2023-09-17 21:04:21 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int num_notas;
-float promedio_final,nota, suma_notas;
-Console.Write("Ingrese el número de notas: ");
-num_notas = int.Parse(Console.ReadLine());
-suma_notas = 0;
-for (int i = 1; i <= num_notas; i++)
+double limite,factorial,opción;
+Console.Write("Ingrese el número límite: ");
+limite = double.Parse(Console.ReadLine());
+Console.WriteLine("============================");
+Console.WriteLine("Escoja una opción: ");
+Console.WriteLine("(1) Números pares ");
+Console.WriteLine("(2) Números impares ");
+Console.WriteLine("(3) Factorial ");
+opción = double.Parse(Console.ReadLine());
+switch (opción)
 {
-    Console.Write("Ingrese la nota " + i + ": ");
-    nota = int.Parse(Console.ReadLine());
-    suma_notas = suma_notas + nota;
+	case 1:
+		Console.Write("Los números pares son: ");
+		for (int i = 1; i <= limite; i++)
+		{
+			if (i % 2 == 0)
+			{
+				Console.Write(i + ", ");
+			}
+		}
+		break;
+
+	case 2:
+        Console.Write("Los números impares son: ");
+        for (int i = 1; i <= limite; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.Write(i + ", ");
+            }
+        }
+        break;
+
+	case 3:
+		if (limite == 0)
+		{
+			Console.WriteLine("El factorial de {0} es: 1" ,limite);
+		}
+		else
+		{
+			factorial = 1;
+			for (int i = 1; i <= limite; i++)
+			{
+                factorial = factorial * i;
+            }
+			Console.WriteLine("El factorial de {0} es: {1}" ,limite,factorial);
+		}
+		break;
+
+	default:
+		Console.WriteLine("Opción inválida");
+		break;
 }
-promedio_final = suma_notas / num_notas;
-Console.WriteLine("El promedio es " + promedio_final);
