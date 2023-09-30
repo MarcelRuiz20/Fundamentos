5db6e62c4b22f7c575efb11c855465fc6afdb5d5
MarcelRuiz20 <143234045+MarcelRuiz20@users.noreply.github.com>
2023-09-29 20:34:41 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,58 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int num_maximo, cont_actual = 0, opcion, num_personas;
+Console.WriteLine("Hello, World!");
+int num_maximo, cont_actual = 0;
+int vecesLleno = 0, vecesVacio = 0;
 
 Console.Write("Ingrese un número máximo de personas: ");
-num_maximo = int.Parse(Console.ReadLine());
-
-Console.WriteLine("========================================");
-Console.WriteLine($"El número máximo establecido es de {num_maximo} personas,");
-Console.WriteLine("presione una tecla para comenzar a contar.");
-Console.ReadKey();
+num_maximo = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
 
 while (true)
 {
-    Console.WriteLine("========================================");
-    Console.WriteLine($"Personas actualmente en la tienda: {cont_actual}");
-    Console.WriteLine("1. Registrar entrada de personas.");
-    Console.WriteLine("2. Registrar salida de personas.");
-    Console.WriteLine("3. Salir.");
-    Console.Write("Seleccione una opción: ");
-    opcion = Convert.ToInt32(Console.ReadLine());
+    double porcentajeAforo = ((double)cont_actual / num_maximo) * 100;
 
-    switch (opcion)
+    Console.WriteLine($"| Asistentes actuales | {cont_actual}\n| Aforo               | {porcentajeAforo}%\n| Máximo              | {num_maximo} personas\n====================================\nPresione\n[ i ] si ingresa una persona\n[ s ] si sale una persona\n[ x ] para terminar");
+    char opcion = Console.ReadKey().KeyChar;
+
+    if (opcion == 'i' && cont_actual < num_maximo)
     {
-        case 1:
-            Console.Write("Ingrese número de personas que entran: ");
-            num_personas = Convert.ToInt32(Console.ReadLine());
-            if (cont_actual + num_personas <= num_maximo)
-            {
-                cont_actual += num_personas;
-            }
-            else
-            {
-                Console.WriteLine("Superará el límite. No se puede ingresar esa cantidad de personas.");
-            }
-            break;
-
-        case 2:
-            Console.Write("Ingrese número de personas que salen: ");
-            num_personas = Convert.ToInt32(Console.ReadLine());
-            if (cont_actual - num_personas >= 0)
-            {
-                cont_actual -= num_personas;
-            }
-            else
-            {
-                Console.WriteLine("No puede salir más gente de la que está dentro.");
-            }
-            break;
-
-        case 3:
-            Console.WriteLine("Finalizando programa.");
-            return;
-
-        default:
-            Console.WriteLine("Opción no válida.");
-            break;
+        cont_actual++;
+        if (cont_actual == num_maximo) vecesLleno++;
     }
+    else if (opcion == 's' && cont_actual > 0)
+    {
+        cont_actual--;
+        if (cont_actual == 0) vecesVacio++;
+    }
+    else if (opcion == 'x')
+    {
+        Console.Clear();
+        Console.WriteLine($"=================================\nEl programa ha terminado\n====================================\nEstadísticas:\n----------------------------------\n{vecesLleno} veces se llenó el local\nEstuvo vacío {vecesVacio} veces");
+        return;
+    }
+
+    Console.Clear();
 }
