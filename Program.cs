fde383dc1f48c11feeea52836dbe2e6ca3bcce32
MarcelRuiz20 <143234045+MarcelRuiz20@users.noreply.github.com>
2023-11-20 18:16:59 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LAB_13
+namespace LAB_14
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             int opción;
-            int elección_de_nivel;
+            int edad;
             int cont = 0;
-            int[] niveles = new int[100];
+            int cont_si = 0;
+            int cont_no = 0;
+            int[] cont_edades = new int[6];
+            string vacunación;
+            int[] edades = new int[100];
+            string[] vacunas = new string[100];
             int[] array = new int[100];
             for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine("================================");
-                Console.WriteLine("Encuestas de Calidad");
+                Console.WriteLine("Encuesta Covid-19");
                 Console.WriteLine("================================");
-                Console.WriteLine("1: Realizar Encuesta ");
-                Console.WriteLine("2: Ver datos registrados ");
-                Console.WriteLine("3: Eliminar un dato ");
-                Console.WriteLine("4: Ordenar datos de menor a mayor ");
+                Console.WriteLine("1: Realizar Encuesta");
+                Console.WriteLine("2: Mostrar Datos de la encuesta");
+                Console.WriteLine("3: Mostrar Resultados");
+                Console.WriteLine("4: Buscar Personas por edad");
                 Console.WriteLine("5: Salir");
                 Console.WriteLine("================================");
                 Console.Write("Ingrese una opción: ");
@@ -32,185 +38,126 @@ namespace LAB_13
                 switch (opción)
                 {
                     case 1:
+                        Console.WriteLine("================================");
+                        Console.WriteLine("Encuesta de vacunación");
+                        Console.WriteLine("================================");
+                        Console.Write("¿Qué edad tienes?: ");
+                        edad = int.Parse(Console.ReadLine());
+                        edades[cont] = edad;
+                        if (edad >= 1 && edad <= 20)
+                        {
+                            cont_edades[0]++;
+                        }
+                        if (edad >= 21 && edad <= 30)
+                        {
+                            cont_edades[1]++;
+                        }
+                        if (edad >= 31 && edad <= 40)
+                        {
+                            cont_edades[2]++;
+                        }
+                        if (edad >= 41 && edad <= 50)
+                        {
+                            cont_edades[3]++;
+                        }
+                        if (edad >= 51 && edad <= 60)
+                        {
+                            cont_edades[4]++;
+                        }
+                        if (edad >= 61)
+                        {
+                            cont_edades[5]++;
+                        }
+                        Console.WriteLine("------------------");
+                        Console.WriteLine("¿Te has vacunado?");
+                        Console.WriteLine("Sí");
+                        Console.WriteLine("No");
                         Console.WriteLine("===================================");
-                        Console.WriteLine("Nivel de Satisfacción ");
-                        Console.WriteLine("===================================");
-                        Console.WriteLine("¿Qué tan satisfecho está con la");
-                        Console.WriteLine("atención de nuestra tienda?");
-                        Console.WriteLine("-----------------------------------");
-                        Console.WriteLine("1: Nada satisfecho");
-                        Console.WriteLine("2: No muy satisfecho");
-                        Console.WriteLine("3: Tolerable");
-                        Console.WriteLine("4: Satisfecho");
-                        Console.WriteLine("5: Muy satisfecho");
-                        Console.WriteLine("===================================");
-                        Console.Write("Ingrese una opción: ");
-                        elección_de_nivel = int.Parse(Console.ReadLine());
-                        niveles[cont] = elección_de_nivel;
+                        Console.Write("Escriba una opción: ");
+                        vacunación = Console.ReadLine();
+                        vacunas[cont] = vacunación;
                         cont++;
+                        if (vacunación == "Si" || vacunación == "si" || vacunación == "SI")
+                        {
+                            cont_si++;
+                        }
+                        else
+                        {
+                            cont_no++;
+                        }
                         Console.Clear();
-                        Console.WriteLine("=================================== ");
-                        Console.WriteLine("Nivel de Satisfacción ");
-                        Console.WriteLine("=================================== ");
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine("¡Gracias por participar!");
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine("=================================== ");
-                        Console.WriteLine("Presione una tecla para");
-                        Console.WriteLine("regresar al menú . . .");
+                        Console.WriteLine("===================================");
+                        Console.WriteLine("Encuesta de vacunación");
+                        Console.WriteLine("===================================\n\n");
+                        Console.WriteLine("¡Gracias por participar!\n\n");
+                        Console.WriteLine("===================================");
+                        Console.WriteLine("Presione una tecla ...");
                         Console.ReadKey();
                         Console.Clear();
                         break;
-                    case 2:
-                        int num,nivel_1 = 0,nivel_2 = 0,nivel_3 = 0,nivel_4 = 0,nivel_5 = 0;
+                        case 2:
                         Console.WriteLine("===================================");
-                        Console.WriteLine("Ver datos registrados");
-                        Console.WriteLine("===================================");
+                        Console.WriteLine("Datos de la encuesta");
+                        Console.WriteLine("===================================\n");
+                        Console.WriteLine("  ID | Edad | Vacunado");
+                        Console.WriteLine("=======================");
                         for (int j = 0; j < cont; j++)
                         {
-                            if (j % 5 == 0 && j != 0)
-                            {
-                                Console.WriteLine();
-                            }
-                            Console.Write("["+niveles[j]+ "] ");
+                            Console.WriteLine("   "+j +" |  "+ edades[j]+ "  |   " + vacunas[j]);
                         }
-
-                        for (int j = 0; j < niveles.Length; j++)
-                        {
-                            num = niveles[j];
-                            if (num == 1)
-                            {
-                                nivel_1++;
-                            }
-                            if (num == 2)
-                            {
-                                nivel_2++;
-                            }
-                            if (num == 3)
-                            {
-                                nivel_3++;
-                            }
-                            if (num == 4) 
-                            {
-                                nivel_4++;
-                            }
-                            if (num == 5)
-                            {
-                                nivel_5++;
-                            }
-                        }
-                        Console.WriteLine();
-                        Console.WriteLine("-----------------------------------");
-                        Console.WriteLine(" " + nivel_1 + " personas: Nada satisfecho");
-                        Console.WriteLine(" " + nivel_2 + " personas: No muy satisfecho");
-                        Console.WriteLine(" " + nivel_3 + " personas: Tolerable");
-                        Console.WriteLine(" " + nivel_4 + " personas: Satisfecho");
-                        Console.WriteLine(" " + nivel_5 + " personas: Muy satisfecho");
                         Console.WriteLine();
                         Console.WriteLine("===================================");
                         Console.WriteLine("Presione una tecla para regresar ...");
                         Console.ReadKey();
                         Console.Clear();
                         break;
-                    case 3:
-                        int posición;
+                        case 3:
                         Console.WriteLine("===================================");
-                        Console.WriteLine("Eliminar un dato");
+                        Console.WriteLine("Resultados de la encuesta");
                         Console.WriteLine("===================================\n");
-                        for (int j = 0; j < cont; j++)
-                        {
-                            if (j % 5 == 0 && j != 0)
-                            {
-                                Console.WriteLine();
-                            }
-                            Console.Write(j + ":[" + niveles[j] + "] ");
-                        }
-                        Console.WriteLine("\n");
-                        Console.WriteLine("===================================");
-                        Console.Write("Ingrese la posición a eliminar: ");
-                        posición = int.Parse(Console.ReadLine());
-                        int[] NuevoNiveles = new int[niveles.Length - 1]; 
-
-                        for (int j = 0; j < niveles.Length - 1; j++)
-                        {
-                            if (posición > j)
-                            {
-                                NuevoNiveles[j] = niveles[j];
-                            }
-                            else
-                            {
-                                NuevoNiveles[j] = niveles[j + 1];
-                            }
-                        }
-                        Console.WriteLine("===================================\n");
-                        for (int j = 0; j < cont - 1; j++)
-                        {
-                            if (j % 5 == 0 && j != 0)
-                            {
-                                Console.WriteLine();
-                            }
-                            Console.Write(j + ":[" + NuevoNiveles[j] + "] ");
-                        }
-                        Console.WriteLine("\n");
-                        Console.WriteLine("===================================");
-                        Console.WriteLine("Presione una tecla para regresar ...");
-                        Console.ReadKey();
-                        Console.Clear();
-                            break;
-                    case 4:
-                        Console.WriteLine("===================================");
-                        Console.WriteLine("Ordenar datos");
-                        Console.WriteLine("===================================\n");
-                        Console.WriteLine("Datos ingresados:\n");
-                        for (int j = 0; j < cont; j++)
-                        {
-                            if (j % 5 == 0 && j != 0)
-                            {
-                                Console.WriteLine();
-                            }
-                            Console.Write(j + ":[" + niveles[j] + "] ");
-                        }
-                        Console.WriteLine("\n");
-                        Console.WriteLine("===================================");
-                        Console.WriteLine("Ordenando ...");
-                        Console.WriteLine("===================================\n");
-                        for (int a = 0; a < cont; a++)
-                        {
-                            for (int j = a + 1; j < cont; j++)
-                            {
-                                int aux;
-                                if (niveles[a] > niveles[j])
-                                {
-                                    aux = niveles[a];
-                                    niveles[a] = niveles[j];
-                                    niveles[j] = aux;
-                                }
-                            }
-                        }
-                        Console.ReadKey();
-                        Console.WriteLine("Datos ordenados:\n");
-                        for (int j = 0; j < cont; j++)
-                        {
-                            if (j % 5 == 0 && j != 0)
-                            {
-                                Console.WriteLine();
-                            }
-                            Console.Write(j + ":[" + niveles[j] + "] ");
-                        }
-
-                        Console.WriteLine("\n");
+                        Console.WriteLine(cont_si + " personas se han vacunado");
+                        Console.WriteLine(cont_no + " personas no se han vacunado\n");
+                        Console.WriteLine("Existen:");
+                        Console.WriteLine(cont_edades[0] + " personas entre 01 y 20 años");
+                        Console.WriteLine(cont_edades[1] + " personas entre 21 y 30 años");
+                        Console.WriteLine(cont_edades[2] + " personas entre 31 y 40 años");
+                        Console.WriteLine(cont_edades[3] + " personas entre 41 y 50 años");
+                        Console.WriteLine(cont_edades[4] + " personas entre 51 y 60 años");
+                        Console.WriteLine(cont_edades[5] + " personas de más de 61 años\n");
                         Console.WriteLine("===================================");
                         Console.WriteLine("Presione una tecla para regresar ...");
                         Console.ReadKey();
                         Console.Clear();
                         break;
-                    
+                        case 4:
+                        cont_si = 0;
+                        cont_no = 0;
+                        Console.WriteLine("================================");
+                        Console.WriteLine("Busca a personas por edad");
+                        Console.WriteLine("================================");
+                        Console.Write("¿Qué edad quieres buscar?: ");
+                        edad = int.Parse(Console.ReadLine());
+                        for (int j = 0; j < cont; j++)
+                        {
+                            if (edad == edades[j] && (vacunas[j] == "Si" || vacunas [j] == "si" || vacunas[j] == "SI"))
+                            {
+                                cont_si++;
+                            }
+                            if (edad == edades[j] && (vacunas[j] == "No" || vacunas[j] == "no" || vacunas[j] == "NO"))
+                            {
+                                cont_no++;
+                            }
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine(cont_si + " se vacunaron");
+                        Console.WriteLine(cont_no + " no se vacunaron\n");
+                        Console.WriteLine("================================");
+                        Console.WriteLine("Presione una tecla para regresar ...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
                 }
-
             }
         }
-
     }
 }
